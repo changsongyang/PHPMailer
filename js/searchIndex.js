910c24bdcab0ac13dddf6aed39b1e80b848a46be
Synchro <marcus@synchromedia.co.uk>
2026-03-24 14:43:58 +0000

    Deploying to gh-pages from @ PHPMailer/PHPMailer@1bb64f37256c5de7fa72a8679a1a957b9b2a4f8e 🚀

--- js/searchIndex.js
+++ js/searchIndex.js
@@ -988,7 +988,7 @@ Search.appendIndex(
         },                {
             "fqsen": "\\PHPMailer\\PHPMailer\\PHPMailer\u003A\u003A\u0024IcalMethods",
             "name": "IcalMethods",
-            "summary": "Value\u002Darray\u0020of\u0020\u0022method\u0022\u0020in\u0020Contenttype\u0020header\u0020\u0022text\/calendar\u0022",
+            "summary": "Value\u002Darray\u0020of\u0020\u0022method\u0022\u0020in\u0020Content\u002Dtype\u0020header\u0020\u0022text\/calendar\u0022",
             "url": "classes/PHPMailer-PHPMailer-PHPMailer.html#property_IcalMethods"
         },                {
             "fqsen": "\\PHPMailer\\PHPMailer\\PHPMailer\u003A\u003A\u0024MIMEBody",
